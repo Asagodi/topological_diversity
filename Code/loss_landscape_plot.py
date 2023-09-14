fdb7225a0c94ee2575b6167ccc7c46d91aa09eb7
Asagodi <abel.sagodi@gmail.com>
2023-09-14 10:36:44 +0100

    noise types and sources for loss landscape

--- Code/loss_landscape_plot.py
+++ Code/loss_landscape_plot.py
@@ -26,12 +26,12 @@ import matplotlib as mpl
 
 from models import mse_loss_masked
 from perturbed_training import RNN
-from tasks import bernouilli_integration_task
+from tasks import bernouilli_integration_task, bernouilli_noisy_integration_task
 
 
 device = 'cpu'
 
-def get_params_perfectintegrator(version, ouput_bias_value=1):
+def get_params_perfectintegrator(version, ouput_bias=1):
     
     a = 1
     
@@ -48,22 +48,46 @@ def get_params_perfectintegrator(version, ouput_bias_value=1):
         wrec_init = np.array([[0,1],[1,0]], dtype=float)
         brec_init = np.array([0,0])
         wo_init = np.array([[1,1]])/(2*a)
-        bwo_init = np.array([-ouput_bias_value])/a
-        h0_init = ouput_bias_value*np.array([1,1])
+        bwo_init = np.array([-ouput_bias])/a
+        h0_init = ouput_bias*np.array([1,1])
     
     elif version=='bla': # V3
         wi_init = a*np.array([[-1,1],[1,-1]], dtype=float)
         wrec_init = np.array([[0,-1],[-1,0]], dtype=float)
-        brec_init = ouput_bias_value*np.array([1,1])
+        brec_init = ouput_bias*np.array([1,1])
         wo_init = np.array([[1,-1]])/(2*a)
         bwo_init = np.array([.0])
-        h0_init = ouput_bias_value/2*np.array([1,1])
+        h0_init = ouput_bias/2*np.array([1,1])
         
 
         
     return wi_init, wrec_init, brec_init, wo_init, bwo_init, h0_init
 
-def loss_landscape(T, input_length, batch_size, ouput_bias_value=1):
+def loss_landscape(T, input_length, batch_size=128, ouput_bias=1, noise_in='weights'):
+    """
+    
+
+    Parameters
+    ----------
+    T : TYPE
+        DESCRIPTION.
+    input_length : TYPE
+        DESCRIPTION.
+    batch_size :  int, optional
+    ouput_bias_value : int, optional
+        output bias for the LAs. The default is 1.
+    noise_in : str, optional
+        noise_in determines the type of noise. The default is 'weights', which corresponds to one source of D-type noise.
+        Other sources are 
+        - 'internal', which corresponds to another source of D-type noise.
+        - 'input', which corresponds to one source of S-type noise.
+
+    Returns
+    -------
+    loss_theta : TYPE
+        DESCRIPTION.
+
+    """
     np.random.seed(10)
     
     thetas = np.logspace(-5,-2, 30)
@@ -81,17 +105,25 @@ def loss_landscape(T, input_length, batch_size, ouput_bias_value=1):
     target = _target.to(device=device).float() 
     mask = _mask.to(device=device).float() 
     
-    
+    noise_std = 0
     model_names = ['irnn', 'ubla', 'bla']
     for model_name_j, model_name in enumerate(model_names):
-        wi_init, wrec_init, brec_init, wo_init, bwo_init, h0_init = get_params_perfectintegrator(model_name, ouput_bias_value=ouput_bias_value)
+        wi_init, wrec_init, brec_init, wo_init, bwo_init, h0_init = get_params_perfectintegrator(model_name, ouput_bias=ouput_bias)
         
-
+        
         for theta_i,theta in enumerate(thetas):
-            wrec_init_p = wrec_init.copy()
-            wrec_init_p[0,0] += theta
             
-            net = RNN(dims=(2,2,1), noise_std=0, dt=1,
+            wrec_init_p = wrec_init.copy()
+            if noise_in=='weights':
+                wrec_init_p[0,0] += theta
+            
+            if noise_in=='internal':
+                noise_std=theta
+                
+            if noise_in=='input':
+                bernouilli_noisy_integration_task(T=T,input_length=input_length, sigma=theta)
+            
+            net = RNN(dims=(2,2,1), noise_std=noise_std, dt=1,
                       nonlinearity='relu', readout_nonlinearity='id',
                       wi_init=wi_init, wrec_init=wrec_init_p, wo_init=wo_init, brec_init=brec_init, bwo_init=bwo_init,
                       h0_init=h0_init, ML_RNN=True)
@@ -117,11 +149,15 @@ def loss_landscape(T, input_length, batch_size, ouput_bias_value=1):
     ax.set_ylabel('loss')
     ax.legend(title='Network')
     ax.grid()
-    plt.savefig(parent_dir+f"/Stability/figures/loss_landscape_bias{ouput_bias_value}.pdf", bbox_inches="tight")
+    plt.savefig(parent_dir+f"/Stability/figures/loss_landscape_bias{ouput_bias}_{noise_in}.pdf", bbox_inches="tight")
             
     return loss_theta
 
 if __name__ == "__main__":
     print(current_dir)
     
-    loss_landscape(T=1000, input_length=50, batch_size=128*4*4, ouput_bias_value=25)
+    loss_theta = loss_landscape(T=20, input_length=10, batch_size=1024,
+                                ouput_bias=20, noise_in='internal')
+    
+    mean_loss = np.mean(loss_theta, axis=0)
+    print(mean_loss)
