2ea194b5cb9b4aa2411f954579359b75f7176a2b
Asagodi <abel.sagodi@gmail.com>
2023-09-14 17:15:54 +0100

    line search matching noise, 4 types

--- Code/loss_landscape_plot.py
+++ Code/loss_landscape_plot.py
@@ -32,6 +32,10 @@ from tasks import bernouilli_integration_task, bernouilli_noisy_integration_task
 
 device = 'cpu'
 
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+sns.set_context("notebook", font_scale=1.25, rc={"lines.linewidth": 1})
+
 def get_params_perfectintegrator(version, ouput_bias=1):
     
     a = 1
@@ -184,7 +188,6 @@ def loss_landscape_fixednoise(Ts, input_length, thetas, batch_size=128, ouput_bi
     """
     np.random.seed(10)
     
-    
     losses = np.zeros((Ts.shape[0], 3))
     
     noise_std = 0
@@ -194,8 +197,10 @@ def loss_landscape_fixednoise(Ts, input_length, thetas, batch_size=128, ouput_bi
         wi_init, wrec_init, brec_init, wo_init, bwo_init, h0_init = get_params_perfectintegrator(model_name, ouput_bias=ouput_bias)
         theta = thetas[model_name_j]
         
-        for j in range(2):
-            for T_i, T in enumerate(Ts):
+        for T_i, T in enumerate(Ts):
+            for j in range(2):
+                if j==1 and noise_in!='weights':
+                    break
                 wrec_init_p = wrec_init.copy()
                 if noise_in=='weights':
                     wrec_init_p[0,0] += (-1)**j*theta
@@ -205,6 +210,9 @@ def loss_landscape_fixednoise(Ts, input_length, thetas, batch_size=128, ouput_bi
                     
                 if noise_in=='input':
                     sigma=theta
+                    
+                if noise_in=='weight_decay':
+                    wrec_init_p -= theta*wrec_init_p
                 task = bernouilli_noisy_integration_task(T=T,input_length=input_length, sigma=sigma)
                 _input, _target, _mask = task(batch_size)
                 input = torch.from_numpy(_input).to(device=device).float() 
@@ -218,9 +226,10 @@ def loss_landscape_fixednoise(Ts, input_length, thetas, batch_size=128, ouput_bi
     
                 output = net(input)
                 loss = mse_loss_masked(output, target, mask).item()
-                losses[T_i,model_name_j] += loss/2.
+                losses[T_i,model_name_j] += loss
             
-            
+    if noise_in=='weights':
+        losses /= 2.
     return losses
 
 def plot_loss_landscape(losses, ouput_bias, noise_in):
@@ -246,7 +255,7 @@ def plot_loss_landscape(losses, ouput_bias, noise_in):
     ax.legend(title='Network')
     ax.grid()
     plt.savefig(parent_dir+f"/Stability/figures/matched_loss_landscape_bias{ouput_bias}_{noise_in}.pdf", bbox_inches="tight")
-
+    plt.show()
 
 def calculate_losses(thetas, Ts, input_length, batch_size=128, ouput_bias=1, noise_in='weights'):
     
@@ -259,17 +268,41 @@ def calculate_losses(thetas, Ts, input_length, batch_size=128, ouput_bias=1, noi
     
     return mean_losses
     
+def plot_losses(mean_losses, threshold, ouput_bias, noise_in):
     
+    fig = plt.figure(figsize=(6,4))
+    ax = plt.subplot(111)
+    ax.set_prop_cycle(color=['k', 'crimson', 'b'])
+    
+    ax.axhline(y=threshold, linestyle='--')
+    
+    for i in range(3):
+        ax.plot(thetas, mean_losses[:,i], markers[i], label=labels[i], markersize=markersizes[i], alpha=alphas[i], zorder=-i)
+    
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel(r'$|\alpha|$')
+    ax.set_ylabel('loss')
+    ax.legend(title='Network')
+    ax.grid()
+    plt.savefig(parent_dir+f"/experiments/noisy/matching_T{Ts[-1]}_threshold{threshold}_bias{ouput_bias}_{noise_in}.pdf", bbox_inches="tight")
+    plt.show()
 
 if __name__ == "__main__":
     print(current_dir)
     
-    Ts = np.arange(10, 100, 2)
-    thetas = [2.1e-04, 5e-05, 1e-04]
-    ouput_bias=20
-    noise_in = 'weights'
+    labels=['iRNN', 'UBLA', 'BLA']
+    markers = ['o-', '^-', 's-']
+    markersizes = [5,5,6]
+    alphas = [.5, .5, .5]
+    
+    # Ts = np.arange(10, 100, 2)
+    Ts = np.array([1000])
+    ouput_bias = 20
     input_length = 10
     batch_size = 1024
+    
+    # thetas = [2.1e-04, 5e-05, 1e-04]
     # losses = loss_landscape_fixednoise(Ts=Ts, input_length=input_length, batch_size=batch_size,
     #                            thetas=thetas,
     #                            ouput_bias=ouput_bias, noise_in=noise_in)
@@ -277,7 +310,13 @@ if __name__ == "__main__":
     # loss_theta = loss_landscape(T=200, input_length=10, batch_size=1024,
     #                             ouput_bias=20, noise_in='input')
     
-    thetas = np.logspace(-3, -7, 5)
-    mean_losses = calculate_losses(thetas, Ts, input_length, batch_size=batch_size, ouput_bias=ouput_bias, noise_in=noise_in)
+    thetas = np.logspace(-1, -9, 10)
+    threshold = 1e-5
+    noise_in_list = ['weights', 'input', 'internal',  'weight_decay']
 
-            
+    for noise_in in noise_in_list:
+
+        mean_losses = calculate_losses(thetas, Ts, input_length, batch_size=batch_size, ouput_bias=ouput_bias, noise_in=noise_in)
+
+        plot_losses(mean_losses, threshold, ouput_bias, noise_in)
+    
