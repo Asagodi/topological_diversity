ba0288b50ffd4f7ae474f5f914e5b90ef43e7578
Asagodi <abel.sagodi@gmail.com>
2023-10-06 11:22:06 +0100

    get period

--- Code/noorman.py
+++ Code/noorman.py
@@ -31,24 +31,67 @@ from itertools import chain, combinations, permutations
 from ring_functions import define_ring, ReLU, simulate_ring, get_corners, plot_solution
 from ring_functions import get_bumps_along_oneside_ring, get_all_bumps, simulate_bumps, plot_ring, v_zero, v_constant, v_switch
 
-def plot_unfolded():
+def plot_unfolded(sol):
+    fig, ax = plt.subplots(1, 1, figsize=(5, 2))
+    X_proj2 = pca.transform(sol) 
+    theta = np.arctan2(X_proj2[:,1], X_proj2[:,0])
+    ax.plot(t, theta, '-')
+    ax.set_xlabel("Time")
+    # ax.set_ylim([np.pi/2-.1,np.pi/2+.1])
+    # ax.set_yticks([-np.pi,np.pi],[r"$-\pi$",r"$\pi$"])
+    ax.set_ylabel("Angle (rad)")
     
-    0
-    
-def plot_unfolded_for_inputs():
+def plot_unfolded_for_inputs(input_list, maxT=25, tsteps=501):
     
     fig, ax = plt.subplots(1, 1, figsize=(5, 2))
-    for i in [0.05, .1, .5]:
-        sol,t = simulate_ring(W_sym, W_asym, c_ff, y0=corners[4], v_in=v_constant(value=i),
-                              maxT=277, tsteps=1001)
+    all_thetas = np.zeros((len(input_list), tsteps))
+    for vi, input_velocity in enumerate(input_list):
+        sol,t = simulate_ring(W_sym, W_asym, c_ff, y0=corners[4], v_in=v_constant(value=input_velocity),
+                              maxT=maxT, tsteps=tsteps)
         X_proj2 = pca.transform(sol.T) 
         theta = np.arctan2(X_proj2[:,1], X_proj2[:,0])
-        ax.plot(t, theta, ',', label=i)
+        all_thetas[vi,:] = theta
+        ax.plot(t, theta, ',', label=input_velocity)
     ax.set_xlabel("Time")
     ax.set_yticks([-np.pi,np.pi],[r"$-\pi$",r"$\pi$"])
     ax.set_ylabel("Angle (rad)")
     plt.legend(title='input', loc='lower left', bbox_to_anchor=(1, 0.5))
     plt.savefig(currentdir+f"/Stability/figures/noorman/N{N}_unfolded_inputs.pdf", bbox_inches="tight")
+    return all_thetas
+
+
+
+def get_period(sol):
+    """
+    Estimate the period of the integrated limit cycle through the Fast Fourier Transform
+
+    Parameters
+    ----------
+    sol : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    estimated_period : TYPE
+        DESCRIPTION.
+
+    """
+    # Perform FFT
+    fft_result = np.fft.fft(sol)
+    
+    # Find the dominant frequency
+    # The dominant frequency corresponds to the inverse of the period
+    frequencies = np.fft.fftfreq(sol.shape[0])
+    dominant_frequency = frequencies[np.argmax(np.abs(fft_result))]
+    
+    # Calculate the period from the dominant frequency
+    estimated_period = 1 / dominant_frequency
+    
+    return estimated_period
+    
+
+def get_period_poincaremap(sol, y0):
+    np.where(np.abs(sol[:,0]-y0[0])<.0000001)
 
 cmap = 'gray'
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
@@ -57,7 +100,7 @@ rc('text', usetex=True)
 if __name__ == "__main__":
     np.random.seed(1) #makes pca project to hexagon sitting
     N = 6    
-    tau = 1
+    tau = .1
     c_ff = 1
 
     transfer_function = ReLU
@@ -75,14 +118,29 @@ if __name__ == "__main__":
     pca = sklearn.decomposition.PCA(n_components=2)
     X_proj2 = pca.fit_transform(sols[:,-1,:]) 
     
-    value = .1
+    value = 10
+    maxT= 10/4
+    t_switch= maxT/12
+    tsteps=100001
     # corners[4] is on the right edge
-    sol,t = simulate_ring(W_sym, W_asym, c_ff, y0=corners[4], v_in=v_constant(value=value),
-                          maxT=27.7/value, tsteps=1001)
+    sol,t = simulate_ring(W_sym, W_asym, c_ff, tau=tau, y0=corners[4], v_in=v_switch(values=[value,0], t_switch=t_switch),
+                          maxT=maxT, tsteps=tsteps)
+
+    plot_unfolded(sol.T)
+    # plt.savefig(currentdir+f"/Stability/figures/noorman/N{N}_input{value}_until{np.round(t_switch,2)}.pdf", bbox_inches="tight")
+
+    # sol,t = simulate_ring(W_sym, W_asym, c_ff, y0=corners[4], v_in=v_constant(value=value),
+    #                       maxT=277, tsteps=1001)
     fig, ax, theta = plot_solution(sol.T, corners, pca, lims = 3.)
+    plt.savefig(currentdir+f"/Stability/figures/noorman/N{N}_input{value}_until{np.round(t_switch,2)}.pdf", bbox_inches="tight")
+
     # plt.savefig(currentdir+f"/Stability/figures/noorman/N{N}_input{value}.pdf", bbox_inches="tight")
+    
+    # input_list=[0.5, 1, 2]
+    # all_thetas = plot_unfolded_for_inputs(input_list, maxT=50, tsteps=501)
 
-
+    # period = get_period(sol.T)
+    # print(period)
 
     # fig, ax = plt.subplots(1, 1, figsize=(5, 2))
     # for i in np.logspace():
