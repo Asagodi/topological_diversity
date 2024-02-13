71dcb9c65b8d97c57f7d0ad0e2e0d85e21d930de
Asagodi <abel.sagodi@gmail.com>
2024-02-13 16:16:39 +0000

    save both res and dict

--- Code/models.py
+++ Code/models.py
@@ -750,6 +750,7 @@ def train(net, task=None, data=None, n_epochs=10, batch_size=32, learning_rate=1
                     "weights_init":weights_init, "weights_last":weights_last, "weights_train":weights_train,
                     "epochs":epochs, "rec_epochs":rec_epochs}
 
+    return res, res_dict
 
 def run_net(net, task, batch_size=32, return_dynamics=False, h_init=None):
     # Generate batch
