31b0434c2a7a0143f9bbe0aaf97eaadea4f2d29a
Asagodi <abel.sagodi@gmail.com>
2024-05-18 21:31:09 +0100

    angular_loss_with_input

--- Code/angular_loss_analysis.py
+++ Code/angular_loss_analysis.py
@@ -125,6 +125,32 @@ def angular_loss_noinput(net, angle_init, h_init, T, batch_size=128, dt=0.1, ran
     return mean_error, max_error, min_error
 
 
+def angular_loss_with_input():
+    task = angularintegration_task(T=10, dt=dt, sparsity=1, random_angle_init=False)
+    input, target, mask, output, trajectories = simulate_rnn_with_task(net, task, 100, h_init='random', batch_size=batch_size)
+    xs, csx2, csx2_proj2 = get_manifold_from_closest_projections(trajectories, net.wo.detach().numpy(), npoints=batch_size)
+
+    T1 = result['training_kwargs']['T']/result['training_kwargs']['dt_rnn'];
+    T=int(16*T1)
+
+    net.map_output_to_hidden = False
+    net.noise_std = 0.
+    anlge_init = xs[:-1]
+    input = np.zeros((batch_size, T, 1))
+    stim = np.linspace(-.1, .1, num=batch_size, endpoint=True)
+    input[:,:T,0] = np.repeat(stim,T).reshape((batch_size,T))
+    output, trajectories = simulate_rnn_with_input(net, input, h_init=csx2[:-1,:])
+    outputs_1d = np.cumsum(input, axis=1)*result['training_kwargs']['dt_task']
+    output_angle = np.arctan2(output[:,:,1], output[:,:,0]);
+    target_angle = xs[:-1][:, np.newaxis] + outputs_1d.squeeze()
+    target_angle = (target_angle + np.pi) % (2 * np.pi) - np.pi
+    angle_error = np.abs(output_angle - target_angle)
+    angle_error[np.where(angle_error>np.pi)] = 2*np.pi-angle_error[np.where(angle_error>np.pi)]
+
+    mean_error = np.mean(angle_error,axis=0)
+    max_error = np.max(angle_error,axis=0)
+    min_error = np.min(angle_error,axis=0)
+
 
 def angle_analysis_on_net(net, input_or_task='input',
                           batch_size=128,
@@ -148,4 +174,21 @@ def angle_analysis_on_net(net, input_or_task='input',
     
 
 
+    
+    fig, ax = plt.subplots(1, 1, figsize=(5, 3));
+    plt.plot(max_error[:int(T/2)], color='r', label='$\epsilon^+$');
+    plt.plot(min_error[:int(T/2)], color='g', label='$\epsilon^-$');
+    plt.plot(mean_error[:int(T/2)], color='b', label='$\mathcal{L}(\delta t)$'); 
+    plt.plot(8.5*T1, max_error[-1], '.', color='r');
+    plt.plot(8.5*T1, min_error[-1], '.', color='g');
+    plt.plot(8.5*T1, mean_error[-1], '.', color='b');
+    
+    plt.legend(); plt.ylabel("loss"); plt.xlabel("t");
+    
+    T1 = result['training_kwargs']['T']/result['training_kwargs']['dt_rnn']
+    ax.axvline(T1, linestyle='--', color='r')
+    ax.text(T1*1.15, .8, r'$T_1$',color='r')
+    ax.set_xticks(np.arange(0,9*T1,T1),[0]+[f'$T_{i}$' for i in range(1,9)])
+    #fig.savefig(folder+"/angle_error.pdf", bbox_inches="tight");
+    
     
