06f9880bc18089e2091ad410f61e1d1178c01052
Asagodi <abel.sagodi@gmail.com>
2024-08-21 08:32:49 +0100

    torus plotting

--- Code/torus_sphere.py
+++ Code/torus_sphere.py
@@ -156,3 +156,53 @@ def grid_on_4dtorus(R=1.0, r=0.5, u_res=20j, v_res=20j):
     points = np.vstack((w1_flat, w2_flat, w3_flat, w4_flat)).T
     
     return points
+
+
+#simulate trajectories on torus
+# points = grid_on_4dtorus(u_res=100j, v_res=100j)
+# points_out = points*1.2
+# points_init_nd = np.dot(D, points_out.T).T; Nsims=points_out.shape[0];
+# trajs = simulate_network_ntimes(Nsims, W, 0, nonlinearity_ode=tanh_ode, tau=10, maxT=250, tsteps=5001,
+#   mlrnn=False, y0s=points_init_nd);
+# trajs_proj_outside = np.dot(trajs,D); 
+# points_in = points*0.8
+# points_init_nd = np.dot(D, points_in.T).T; 
+# trajs = simulate_network_ntimes(Nsims, W, 0, nonlinearity_ode=tanh_ode, tau=10, maxT=250, tsteps=5001,
+#   mlrnn=False, y0s=points_init_nd);
+# trajs_proj_inside = np.dot(trajs,D);
+
+#plot torus and fps
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d', computed_zorder=True);
+# x, y, z = torus_4d_to_3d(xstars_2, r1=1, r2=.3)
+# ax.scatter(x, y, z, color=stab_colors[stabilist])
+
+# x, y, z = torus_4d_to_3d(trajs_proj_outside[:,-1,:], r1=1, r2=.3)
+# x = x.reshape((20,20))
+# y = y.reshape((20,20))
+# z = z.reshape((20,20))
+# ax.plot_wireframe(x, y, z, color="k")
+# ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+# ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+# ax.zaxis.set_major_locator(MaxNLocator(integer=True))
+# ax.set_zlim([-1,1]);  plt.savefig(folder+'/torus_fps.pdf')
+
+
+#plot torus wire
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d');
+# x, y, z = torus_4d_to_3d(trajs_proj_outside[:,0,:], r1=1, r2=.4)
+# x = x.reshape((20,20))
+# y = y.reshape((20,20))
+# z = z.reshape((20,20))
+# ax.plot_wireframe(x, y, z, color="b")
+
+# x, y, z = torus_4d_to_3d(trajs_proj_outside[:,-1,:], r1=1, r2=.3)
+# x = x.reshape((20,20))
+# y = y.reshape((20,20))
+# z = z.reshape((20,20))
+# ax.plot_wireframe(x, y, z, color="r")
+# ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+# ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+# ax.zaxis.set_major_locator(MaxNLocator(integer=True))
+# ax.set_zlim([-1,1]); ax.plot_wireframe(x, y, z, color="r"); plt.savefig(folder+'/spheretorus_start_end_big.pdf')
