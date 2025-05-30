02ae5ceb9e782c0d2867ec1b69ae6edc7aa0d94f
Asagodi <abel.sagodi@gmail.com>
2025-05-30 12:11:31 +0100

    reg formatting

--- iam/scripts/utils.py
+++ iam/scripts/utils.py
@@ -17,3 +17,12 @@ def set_seed(seed: int):
     torch.backends.cudnn.benchmark = False  # Disables benchmarking in CUDA (might affect performance)
     # If using a non-deterministic layer (e.g., dropout), you can use the following line:
     torch.use_deterministic_algorithms(True)  # For PyTorch 1.8 and later
+
+
+def format_reg_name(reg: float) -> str:
+    if reg == 0:
+        return "0"
+    elif reg >= 0.1:
+        return f"{reg:.1f}"       # e.g., 1.0 → '1.0', 0.1 → '0.1'
+    else:
+        return f"{reg:.0e}"       # e.g., 0.001 → '1e-3'
